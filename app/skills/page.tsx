78058ad1138a5863f916ba0deaa25559e4f8e220
Karl <KarlUweMartin@gmail.com>
2026-05-09 14:28:52 +0200

    Updated skills page

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -7,6 +7,13 @@ import BrushIcon from '@mui/icons-material/Brush';
 import DevicesOtherIcon from '@mui/icons-material/DevicesOther';
 import CloudSyncIcon from '@mui/icons-material/CloudSync';
 import { useTranslations } from "next-intl";
+import { ReactElement } from "react";
+
+import DrawIcon from '@mui/icons-material/Draw';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import PhonelinkIcon from '@mui/icons-material/Phonelink';
+import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
+
 
 export default function SkillsPage({ id }: { id?: string }){
   const devTools = [
@@ -14,6 +21,7 @@ export default function SkillsPage({ id }: { id?: string }){
     "Visual Studio / VS Code",
     "Mixed Reality Toolkit",
     "React",
+    "React Native",
     "Flutter",
     "Next.js",
     "Figma",
@@ -21,8 +29,7 @@ export default function SkillsPage({ id }: { id?: string }){
   ];
 
   const languages = [
-    "C# / .NET", 
-    "ASP.NET",
+    "C# / .NET / ASP.NET", 
     "JavaScript", 
     "TypeScript", 
     "Dart",
@@ -84,52 +91,90 @@ export default function SkillsPage({ id }: { id?: string }){
       </Stack>
     </Box>
   );
-
-
   
-const SkillArticle = ({ title, paragraphs, chips = [] } : {title: string, paragraphs: string[], chips?: string[]}) => (  
-  <Box sx={{ mb: 2, bgcolor: "background.paper", p: 2, borderRadius: 2 }}>
-    <Typography mb={1} variant="h6">
-      {title}
-    </Typography>
-
-    {paragraphs.map((paragraph, index) => {
-      return (
-        <Typography
-          key={index}
-          component="p"
-          variant="body1"
-          color="text.secondary"
+  const SkillArticle = ({
+    title,
+    paragraphs,
+    chips = [],
+    bgIcon
+  }: {
+    title: string;
+    paragraphs: string[];
+    chips?: string[];
+    bgIcon?: ReactElement
+  }) => (
+    <Box
+      sx={{
+        mb: 2,
+        bgcolor: "background.paper",
+        p: 2,
+        borderRadius: 2,
+        position: "relative",
+        overflow: "hidden",
+      }}
+    >
+      {bgIcon && 
+        <Box
           sx={{
-            mb: 1.5,
-            "& b, & strong": { fontWeight: 700, color: "text.primary" },
+            position: "absolute",
+            right: 25,
+            top: "50%",
+            transform: "translateY(-50%)",
+            pointerEvents: "none",
+            opacity: 0.2,    
+            maskImage:
+            "linear-gradient(to left, rgba(0,0,0,1), rgba(0,0,0,0.1))",
+            WebkitMaskImage:
+            "linear-gradient(to left, rgba(0,0,0,1), rgba(0,0,0,0.1))",
           }}
-        >
-          {paragraph}
-        </Typography>
-      );
-    })}
+          >
+          {bgIcon}
+        </Box>
+      }
 
-    <Divider sx={{ my: 3 }} />
+      <Typography mb={1} variant="h6">
+        {title}
+      </Typography>
 
-    <Stack mt={2} direction="row" spacing={1} flexWrap="wrap" useFlexGap>
-      {chips.map((item) => (
-        <Chip 
-          sx={{ 
-            fontWeight: "bold", 
-            boxShadow: 1 ,
-            minWidth: "100px", 
-            color: "text.secondary", 
-            backgroundColor: "background.default" 
-          }} 
-          key={item} 
-          label={item}  
-        />
-      ))}
-    </Stack>
+      {paragraphs.map((paragraph, index) => {
+        return (
+          <Typography
+            key={index}
+            component="p"
+            variant="body1"
+            color="text.secondary"    
+            sx={{
+              mb: 1.5,
+              "& b, & strong": {
+                fontWeight: 700,
+                color: "text.primary",
+              },
+            }}
+          >
+            {paragraph}
+          </Typography>
+        );
+      })}
 
-  </Box>
-  );  
+      <Divider sx={{ my: 3 }} />
+
+      <Stack mt={2} direction="row" spacing={1} flexWrap="wrap" useFlexGap>
+        {chips.map((item) => (
+          <Chip
+            sx={{
+              fontWeight: "bold",
+              boxShadow: 1,
+              minWidth: "100px",
+              color: "text.secondary",
+              backgroundColor: "background.default",
+            }}
+            key={item}
+            label={item}
+          />
+        ))}
+      </Stack>
+    </Box>
+  );
 
   const t = useTranslations('Skills');
 
@@ -141,21 +186,25 @@ const SkillArticle = ({ title, paragraphs, chips = [] } : {title: string, paragr
         </Typography>
 
        <SkillArticle
+          bgIcon={<DrawIcon sx={{ fontSize: 220 }} />}
           title="User Experience Design"
           paragraphs={[t("ux1")]}
           chips={["Usability", "Design Thinking", "User Focussed"]}/>
 
         <SkillArticle
+          bgIcon={<AssignmentIcon sx={{ fontSize: 220 }} />}
           title="Project Management"         
           paragraphs={[t("management1"), t("management2")]}
           chips={["Product Owner", "Scrum Master"]}/>
 
-        <SkillArticle
+        <SkillArticle  
+          bgIcon={<PhonelinkIcon sx={{ fontSize: 220 }} />}
           title="Frontend & Mobile Development"
           paragraphs={[t("frontend1"), t("frontend2")]}
           chips={["Cross Platform", "Responsive Layout"]}/>
 
         <SkillArticle
+          bgIcon={<VideogameAssetIcon sx={{ fontSize: 220 }} />}
           title="Unity Development"
           paragraphs={[t("unity1"), t("unity2"), t("unity3")]}
           chips={["Automotive Interfaces", "Mixed Reality", "Realtime 3D", "Data Visualization", "Prototyping" ]}/>
