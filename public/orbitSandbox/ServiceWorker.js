f35218f9296588a9787bfa9ae443ddc33f581d17
Karl <KarlUweMartin@gmail.com>
2026-05-14 14:20:07 +0200

    Updated OrbitSandbox to latest and changed it to PWA style player

---
+++ public/orbitSandbox/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Karl-OrbitSandbox-0.1";
+const contentToCache = [
+    "Build/OrbitSandbox_PWA.loader.js",
+    "Build/OrbitSandbox_PWA.framework.js",
+    "Build/OrbitSandbox_PWA.data",
+    "Build/OrbitSandbox_PWA.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
