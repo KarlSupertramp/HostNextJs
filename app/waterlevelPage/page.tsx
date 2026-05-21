933990088b63c9f27019561cb003a406ff1a869f
Karl <KarlUweMartin@gmail.com>
2026-05-21 19:18:59 +0200

    Fix build error

--- app/waterlevelPage/page.tsx
+++ app/waterlevelPage/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { Box, Container } from "@mui/material";
 import Waterlevel from "../components/waterlevel";
 import { BackButton } from "../sections/backbutton";
 import { useSearchParams } from "next/navigation";
 
 export default function WaterlevelPage() {
-    
   const embedded = useSearchParams().get("embedded") === "true";
 
   return <Container maxWidth="lg" >
