7d5c913404fa901e905365799b3329a270b0d7e3
Karl <KarlUweMartin@gmail.com>
2026-05-21 19:27:31 +0200

    Reverted search param handling

--- app/waterlevelPage/page.tsx
+++ app/waterlevelPage/page.tsx
@@ -1,23 +1,15 @@
 'use client';
 
-export const dynamic = 'force-dynamic';
-
 import { Box, Container } from "@mui/material";
 import Waterlevel from "../components/waterlevel";
 import { BackButton } from "../sections/backbutton";
-import { useSearchParams } from "next/navigation";
-import { Suspense } from 'react';
 
 export default function WaterlevelPage() {
-  const embedded = useSearchParams().get("embedded") === "true";
-
   return (
     <Container maxWidth="lg">
-      {!embedded && <BackButton />}
+      <BackButton />
       <Box height={"85vh"}>
-        <Suspense fallback={<div>Loading waterlevel...</div>}>
           <Waterlevel />
-        </Suspense>
       </Box>
     </Container>
   );
