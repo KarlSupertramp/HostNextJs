fd1ad64e5999bfc4c2399677a03170b981a76e8a
Karl <KarlUweMartin@gmail.com>
2026-05-21 19:24:36 +0200

    Added supense boundry to searchParams

--- app/waterlevelPage/page.tsx
+++ app/waterlevelPage/page.tsx
@@ -6,14 +6,19 @@ import { Box, Container } from "@mui/material";
 import Waterlevel from "../components/waterlevel";
 import { BackButton } from "../sections/backbutton";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from 'react';
 
 export default function WaterlevelPage() {
   const embedded = useSearchParams().get("embedded") === "true";
 
-  return <Container maxWidth="lg" >
+  return (
+    <Container maxWidth="lg">
       {!embedded && <BackButton />}
-      <Box height={"85vh"}>        
-        <Waterlevel />
+      <Box height={"85vh"}>
+        <Suspense fallback={<div>Loading waterlevel...</div>}>
+          <Waterlevel />
+        </Suspense>
       </Box>
     </Container>
-  };
+  );
+}
